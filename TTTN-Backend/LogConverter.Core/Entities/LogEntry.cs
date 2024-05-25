a32f36322215a72ed2d6b7ccbd1ffda80a96f9ca
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-05-24 21:13:08 -0300

    Feat: Criado o projeto e separação de responsabilidades.

---
+++ TTTN-Backend/LogConverter.Core/Entities/LogEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogConverter.Core.Entities
+{
+    public class LogEntry
+    {
+        public int Size { get; set; }
+        public int StatusCode { get; set; }
+        public string CacheStatus { get; set; }
+        public string HttpMethod { get; set; }
+        public string UriPath { get; set; }
+        public double ResponseTime { get; set; }
+
+        public string FormatAgora()
+        {
+            return $"\"MINHA CDN\" {HttpMethod} {StatusCode} {UriPath} {Math.Round(ResponseTime)} {Size} {CacheStatus}";
+        }
+    }
+
+}
---
+++ TTTN-Backend/LogConverter.Core/Interfaces/ILogFormatter.cs
@@ -0,0 +1,14 @@
+﻿using LogConverter.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogConverter.Core.Interfaces
+{
+    public interface ILogFormatter
+    {
+        string FormatLogs(IEnumerable<LogEntry> entries);
+    }
+}
